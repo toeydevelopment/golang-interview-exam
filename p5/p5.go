e1dc938bae1f0a06d892630eb3ee9abeec96f4ad
Anirut Kamchai <anirut.workspace@gmail.com>
2021-10-27 12:32:20 +0700

    :zap: p5

--- p5/p5.go
+++ p5/p5.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func horse(name string) {
+	fmt.Printf("%s start", name)
 	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	fmt.Printf("%s done", name)
 }
 
 func run() {
